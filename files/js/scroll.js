39a77945a4ea6073378e539463178f0e17e17e99
Cl-Hanul <hanul9206@outlook.com>
2023-03-12 12:58:18 +0900

    add blog page

---
+++ files/js/scroll.js
@@ -0,0 +1,64 @@
+// Hide Header on on scroll down
+
+var didScroll;
+
+var lastScrollTop = 0;
+
+var delta = 5;
+document.getElementById("fixedNav")
+
+var navbarHeight = $('fixedNav').outerHeight();
+
+$(window).scroll(function(event){
+
+    didScroll = true;
+
+});
+
+setInterval(function() {
+
+    if (didScroll) {
+
+        hasScrolled();
+
+        didScroll = false;
+
+    }
+
+}, 250);
+
+function hasScrolled() {
+
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+
+    if(Math.abs(lastScrollTop - st) <= delta)
+
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+
+    // This is necessary so you never see what is "behind" the navbar.
+
+    if (st > lastScrollTop && st > navbarHeight){
+
+        // Scroll Down
+
+        $('fixedNav').removeClass('nav-down').addClass('nav-up');
+
+    } else {
+
+        // Scroll Up
+
+        if(st + $(window).height() < $(document).height()) {
+
+            $('fixedNav').removeClass('nav-up').addClass('nav-down');
+
+        }
+
+    }
+
+    lastScrollTop = st;
+
+}
